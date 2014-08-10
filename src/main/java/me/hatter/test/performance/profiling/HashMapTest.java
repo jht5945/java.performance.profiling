e6181d322149072ff8f04c03eee2bdd079335215
Hatter Jiang <jht5945@gmail.com>
2014-08-10 17:41:03 +0800

    add HashMapTest

---
+++ src/main/java/me/hatter/test/performance/profiling/HashMapTest.java
@@ -0,0 +1,12 @@
+package me.hatter.test.performance.profiling;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+
+    }
+    
+//    public static String makeRandom() {
+//        
+//    }
+}
